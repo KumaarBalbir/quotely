45e9438e2d5b797e2e92f80f7dc6d4269bc3ae6d
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-07 12:14:58 +0530

    add server endpoint in production to client

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   //paste link of graphql server
-  uri: "http://localhost:4000/graphql",
+  uri: "https://quotely-3khf.onrender.com/graphql",
   cache: new InMemoryCache(),
   headers: {
     authorization: localStorage.getItem("token") || "",
